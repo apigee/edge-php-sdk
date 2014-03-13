a91998410b6ece1ff6761ed3650b738e72b3eae6
Isaias Arellano Delgado <isaias@apigee.com>
2014-03-13 14:51:46 -0600

    add method accessors

--- Apigee/Mint/DataStructures/SupportedCurrency.php
+++ Apigee/Mint/DataStructures/SupportedCurrency.php
@@ -2,7 +2,7 @@
 
 namespace Apigee\Mint\DataStructures;
 
-class SupportedCurrency
+class SupportedCurrency extends DataStructure
 {
 
     /**
@@ -57,6 +57,146 @@ class SupportedCurrency
      * @var float Minimum amount a developer can set a recurring amount
      */
     public $minimumRecurringAmount = 0;
+
+  /**
+   * @param float $creditLimit
+   */
+  public function setCreditLimit($creditLimit) {
+    $this->creditLimit = $creditLimit;
+  }
+
+  /**
+   * @return float
+   */
+  public function getCreditLimit() {
+    return $this->creditLimit;
+  }
+
+  /**
+   * @param string $description
+   */
+  public function setDescription($description) {
+    $this->description = $description;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDescription() {
+    return $this->description;
+  }
+
+  /**
+   * @param string $displayName
+   */
+  public function setDisplayName($displayName) {
+    $this->displayName = $displayName;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDisplayName() {
+    return $this->displayName;
+  }
+
+  /**
+   * @param string $id
+   */
+  public function setId($id) {
+    $this->id = $id;
+  }
+
+  /**
+   * @return string
+   */
+  public function getId() {
+    return $this->id;
+  }
+
+  /**
+   * @param float $minimumRecurringAmount
+   */
+  public function setMinimumRecurringAmount($minimumRecurringAmount) {
+    $this->minimumRecurringAmount = $minimumRecurringAmount;
+  }
+
+  /**
+   * @return float
+   */
+  public function getMinimumRecurringAmount() {
+    return $this->minimumRecurringAmount;
+  }
+
+  /**
+   * @param string $name
+   */
+  public function setName($name) {
+    $this->name = $name;
+  }
+
+  /**
+   * @return string
+   */
+  public function getName() {
+    return $this->name;
+  }
+
+  /**
+   * @param \Apigee\Mint\Organization $organization
+   */
+  public function setOrganization($organization) {
+    $this->organization = $organization;
+  }
+
+  /**
+   * @return \Apigee\Mint\Organization
+   */
+  public function getOrganization() {
+    return $this->organization;
+  }
+
+  /**
+   * @param string $status
+   */
+  public function setStatus($status) {
+    $this->status = $status;
+  }
+
+  /**
+   * @return string
+   */
+  public function getStatus() {
+    return $this->status;
+  }
+
+  /**
+   * @param mixed $var
+   */
+  public function setVar($var) {
+    $this->var = $var;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getVar() {
+    return $this->var;
+  }
+
+  /**
+   * @param boolean $virtualCurrency
+   */
+  public function setVirtualCurrency($virtualCurrency) {
+    $this->virtualCurrency = $virtualCurrency;
+  }
+
+  /**
+   * @return boolean
+   */
+  public function isVirtualCurrency() {
+    return $this->virtualCurrency;
+  }
 
 
     /**
