a460be1bbed86fd65a3bdd04f34f389087b47775
Daniel Johnson <djohnson@apigee.com>
2014-09-03 11:33:10 -0400

    Whitespace nitpicks

--- Apigee/Drupal/WatchdogLogger.php
+++ Apigee/Drupal/WatchdogLogger.php
@@ -150,18 +150,18 @@ class WatchdogLogger extends \Psr\Log\AbstractLogger
    */
   private function interpolate($message, array $context = array())
   {
-    // build a replacement array with braces around the context keys
-    $replace = array();
-    foreach ($context as $key => $val) {
-      if (!is_scalar($val)) {
-        if (is_object($val) && method_exists($val, '__toString')) {
-          $val = $val->__toString();
-        }
-        else {
-          $val = print_r($val, TRUE);
-        }
-      }
-      $replace['{' . $key . '}'] = $val;
+      // build a replacement array with braces around the context keys
+      $replace = array();
+      foreach ($context as $key => $val) {
+          if (!is_scalar($val)) {
+              if (is_object($val) && method_exists($val, '__toString')) {
+                  $val = $val->__toString();
+              }
+              else {
+                  $val = print_r($val, TRUE);
+              }
+          }
+          $replace['{' . $key . '}'] = $val;
     }
 
     // interpolate replacement values into the message and return
